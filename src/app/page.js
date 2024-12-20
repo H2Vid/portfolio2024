70986abe9bfe0f5529cd1c6e25c704cde948d88d
H2Vid <dstmpl17@gmail.com>
2024-12-21 01:08:44 +0700

    menampilkan semua component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
+import AboutMe from "@/components/aboutMe/AboutMe"
+import Contact from "@/components/contact/Contact"
+import Experience from "@/components/experience/Experience"
 import HeroSection from "@/components/heroSection/HeroSection"
+import { Project } from "@/components/proyek/Project"
 import { SetTools } from "@/components/setTools/SetTools"
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
+      <AboutMe />
+      <Experience />
+      <Project />
       <SetTools />
+      <Contact />
     </>
   )
 }

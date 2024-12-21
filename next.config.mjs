f84545b8064ea3e50e1d7463f60f78e6898d7bbd
H2Vid <dstmpl17@gmail.com>
2024-12-21 20:28:15 +0700

    mengahpus domain Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "assets.aceternity.com"],
-  },
   devIndicators: {
     appIsrStatus: false,
   },
